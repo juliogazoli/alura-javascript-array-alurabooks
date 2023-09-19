d61402e232e4b8512a7fb81e65faaeb793d41ab0
juliogazoli <julio_gazoli@hotmail.com>
2023-09-19 12:37:56 -0300

    Como funciona o filter
    - Filter

---
+++ app/metodoFilter.js
@@ -0,0 +1,8 @@
+const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
+
+btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros)
+
+function filtrarLivros() {
+    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+    console.table(livrosFiltrados)
+}
